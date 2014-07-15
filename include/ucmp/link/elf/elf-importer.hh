2548bda82817aedddb3624bbcab0acfb19f4e621
Baptiste Covolato <b.covolato@gmail.com>
2014-07-16 00:17:34 +0200

    [LINK][ELF] Add import of shstrtab section

--- include/ucmp/link/elf/elf-importer.hh
+++ include/ucmp/link/elf/elf-importer.hh
@@ -19,6 +19,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef UCMP_LINK_ELF_IMPORTER_HH
 # define UCMP_LINK_ELF_IMPORTER_HH
 
+# include <iostream>
+
 # include <ucmp/link/core/importer.hh>
 # include <ucmp/misc/memory-buffer.hh>
 # include <ucmp/misc/elf.hh>
@@ -42,12 +44,20 @@ namespace ucmp
                 class ElfInnerImporter
                 {
                     typedef typename ElfTraits<Elf>::Ehdr Ehdr;
+                    typedef typename ElfTraits<Elf>::Shdr Shdr;
 
                     public:
                         File* parse_file(misc::MemoryBuffer& buf);
 
                     private:
-                        const Ehdr* header_;
+                        void parse_section(File* f, misc::MemoryBuffer& buf,
+                                           const Shdr* section);
+                        void locate_shstr(misc::MemoryBuffer& buf,
+                                          const Ehdr* elf);
+                        std::string shname_get(int index);
+
+                    private:
+                        const char *shstrtab_;
                 };
 
                 typedef ElfInnerImporter<LinkContext::BITS32>
