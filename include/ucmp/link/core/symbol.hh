fb63fcd4cfb9e81245fc9e05e7339d7cc349a5b6
Baptiste Covolato <b.covolato@gmail.com>
2014-07-22 21:26:03 +0200

    [LINK] Add Symbol class

---
+++ include/ucmp/link/core/symbol.hh
@@ -0,0 +1,85 @@
+/*
+Copyright (C) 2014 Baptiste Covolato <b.covolato@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef UCMP_LINK_SYMBOL_HH
+# define UCMP_LINK_SYMBOL_HH
+
+# include <cstdint>
+
+# include <ucmp/misc/symbol.hh>
+
+namespace ucmp
+{
+    namespace link
+    {
+        class Symbol
+        {
+            public:
+                enum Type
+                {
+                    T_NONE,
+                    T_OBJ,
+                    T_CODE,
+                    T_FRAG,
+                };
+
+                enum Binding
+                {
+                    B_NONE,
+                    B_GLOBAL,
+                    B_LOCAL,
+                    B_WEAK,
+                };
+
+            public:
+                Symbol(const misc::Symbol& name,
+                       uint64_t value,
+                       uint64_t size,
+                       const misc::Symbol& frag_name,
+                       Type type,
+                       Binding binding);
+                ~Symbol() = default;
+
+                const misc::Symbol& name_get() const { return name_; }
+                const misc::Symbol& frag_name_get() const
+                {
+                    return frag_name_;
+                }
+
+                uint64_t value_get() const { return value_; }
+                uint64_t size_get() const { return size_; }
+
+                Type type_get() const { return type_; }
+                Binding binding_get() const { return binding_; }
+
+            private:
+                misc::Symbol name_;
+                misc::Symbol frag_name_;
+
+                // Both stored with 64bits but will be truncated for 32bits
+                // by exporter if needed
+                uint64_t value_;
+                uint64_t size_;
+
+                Type type_;
+                Binding binding_;
+        };
+    } // namespace link
+} // namespace ucmp
+
+#endif /* !UCMP_LINK_SYMBOL_HH */
---
+++ lib/link/core/symbol.cc
@@ -0,0 +1,36 @@
+/*
+Copyright (C) 2014 Baptiste Covolato <b.covolato@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include <ucmp/link/core/symbol.hh>
+
+using namespace ucmp;
+using namespace link;
+
+Symbol::Symbol(const misc::Symbol& name,
+               uint64_t value,
+               uint64_t size,
+               const misc::Symbol& frag_name,
+               Type type,
+               Binding binding)
+    : name_(name)
+    , frag_name_(frag_name)
+    , value_(value)
+    , size_(size)
+    , type_(type)
+    , binding_(binding)
+{}
